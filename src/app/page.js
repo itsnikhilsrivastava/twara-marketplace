acfcae4d9e5394fb13e381ea4047753f4ee289d7
itsnikhilsrivastava <nsnowlive@gmail.com>
2026-05-11 13:06:02 +0530

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#2563eb' }}>Twara Marketplace</h1>
+      <p>जल्द ही आ रहा है...</p>
+      <a href="/add-product" style={{ color: 'blue', textDecoration: 'underline' }}>
+        सामान जोड़ें (Vendor Panel)
+      </a>
+    </div>
+  )
+}
